818d23dd464b332770e988b92b0cc246701eda33
JamJomJim <thejamesewens@gmail.com>
2022-06-05 18:28:53 -0500

    Basic simpleweb3 provider use

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { SimpleWeb3Provider } from "simple-web3-sdk"
+import "./App.css"
+import logo from "./logo.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <SimpleWeb3Provider
+      settings={{
+        apiKey: process.env.API_KEY,
+        isTesting: true,
+      }}
+    >
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </SimpleWeb3Provider>
+  )
 }
 
-export default App;
+export default App
