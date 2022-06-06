6abe60c4db0774adbcf0971e3855e2807cfea5c0
JamJomJim <thejamesewens@gmail.com>
2022-06-05 22:59:22 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { SimpleWeb3Provider } from "simple-web3-sdk"
 import "./App.css"
-import logo from "./logo.svg"
+import { ConnectView } from "./ConnectView"
 
 function App() {
   return (
@@ -11,22 +11,7 @@ function App() {
         isTesting: true,
       }}
     >
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ConnectView />
     </SimpleWeb3Provider>
   )
 }
